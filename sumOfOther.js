d1b1333eaa7a1b2152840a21cb7b07ee25cae03c
Sokolov V <sokolov.stb@gmail.com>
2018-10-27 23:18:13 +0300

    feat: inmplement function that returns sum of other array elemtns

---
+++ sumOfOther.js
@@ -0,0 +1,13 @@
+function sumOfOther(array){
+  const arr = array.slice();
+
+  let sum = arr.reduce(function(sum, item){
+    return sum + item;
+  },0);
+
+  for(let i = 0; i < arr.length; i++){
+    arr[i] = sum - arr[i];
+  }
+
+  return arr;
+}
